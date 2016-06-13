7a8659dbca2471c9dd18f03391250d5495ec0cac
alumne <alexbonavila@iesebre.com>
2016-06-13 22:42:12 +0200

    implementant chat de sortida + local storage

--- src/components/Pushwoosh.vue
+++ src/components/Pushwoosh.vue
@@ -2,8 +2,6 @@
     <div class="hello">
         <h1>{{ msg }}</h1>
 
-
-
         <div class="box box-success direct-chat direct-chat-success">
             <div class="box-header with-border">
                 <h3 class="box-title">Direct Chat</h3>
@@ -12,25 +10,24 @@
                     <span data-toggle="tooltip" title="3 New Messages" class="badge bg-green">3</span>
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                     </button>
-                    <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
-                        <i class="fa fa-comments"></i></button>
                     <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                 </div>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
+
                 <!-- Conversations are loaded here -->
                 <div class="direct-chat-messages">
                     <!-- Message. Default to the left -->
-                    <div class="direct-chat-msg">
+                    <div class="direct-chat-msg" v-for="notification in notifications">
                         <div class="direct-chat-info clearfix">
-                            <span class="direct-chat-name pull-left">Alexander Pierce</span>
-                            <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                            <span class="direct-chat-name pull-left">Pushwoosh</span>
+                            <span class="direct-chat-timestamp pull-right">{{ notification.date }}</span>
                         </div>
                         <!-- /.direct-chat-info -->
                         <img class="direct-chat-img" src="bower_components/AdminLTE/dist/img/user1-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
-                                Is this template really for free? That's unbelievable!
+                                {{ notification.message }}
                             </div>
                         <!-- /.direct-chat-text -->
                     </div>
@@ -38,46 +35,19 @@
                 </div>
                 <!--/.direct-chat-messages-->
 
-                <!-- Contacts are loaded here -->
-                <div class="direct-chat-contacts">
-                    <ul class="contacts-list">
-                        <li>
-                            <a href="#">
-                                <img class="contacts-list-img" src="bower_components/AdminLTE/dist/img/user1-128x128.jpg">
 
-                                    <div class="contacts-list-info">
-                            <span class="contacts-list-name">
-                              Count Dracula
-                              <small class="contacts-list-date pull-right">2/28/2015</small>
-                            </span>
-                                        <span class="contacts-list-msg">How have you been? I was...</span>
-                                    </div>
-                                <!-- /.contacts-list-info -->
-                            </a>
-                        </li>
-                        <!-- End Contact Item -->
-                    </ul>
-                    <!-- /.contatcts-list -->
-                </div>
-                <!-- /.direct-chat-pane -->
             </div>
             <!-- /.box-body -->
             <div class="box-footer">
-                <form action="#" method="post">
-                    <div class="input-group">
-                        <input type="text" name="message" placeholder="Type Message ..." class="form-control">
-                      <span class="input-group-btn">
-                        <button type="submit" class="btn btn-success btn-flat">Send</button>
-                      </span>
-                    </div>
-                </form>
+                <div class="input-group">
+                    <input id="message" type="text" name="message" placeholder="Type Message ..." class="form-control">
+                  <span class="input-group-btn">
+                    <button v-on:click="notify" type="button" class="btn btn-success btn-flat">Send</button>
+                  </span>
+                </div>
             </div>
             <!-- /.box-footer-->
         </div>
-
-
-
-
 
     </div>
 </template>
@@ -86,11 +56,34 @@
     export default {
     data () {
     return {
-    // note: changing this line won't causes changes
-    // with hot-reload because the reloaded component
-    // preserves its current state and we are modifying
-    // its initial state.
-    msg: 'Hello World!'
+    msg: 'Hello World!',
+    notification_message: 'Jolo Compolo',
+    notifications: notificationStorage.fetch()
+}
+},
+    methods: {
+    notify: function() {
+    var notification = {message:$('#message').val(), date:"Now"};
+    this.notifications.push(notification);
+    notificationStorage.save(this.notifications);
+    $.ajax({
+    type: "POST",
+    url: "https://cp.pushwoosh.com/json/1.3/createMessage",
+    data: JSON.stringify({
+    "request": {
+    "application": "4FC89B6D14A655.46488481",
+    "auth": "mTdns0j6qLYPa/A5htmD46xVyoxdVQfPBz7NRqYYHz9PhvKXgJtOkAY+yo0YTXDEoztQAJFY0JmXnd89tf59",
+    "notifications": [{
+    "send_date": notification.date,
+    "ignore_user_timezone": true,
+    "content": notification.message
+}]
+}
+}),
+    dataType: "json"
+}).done(function(data) {
+    console.log(data);
+});
 }
 }
 }
